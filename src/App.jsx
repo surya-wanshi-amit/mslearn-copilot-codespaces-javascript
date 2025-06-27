0057829428217d17ddd96b96384724a827d6f034
Amit Suryawanshi <70681440+surya-wanshi-amit@users.noreply.github.com>
2025-06-27 14:44:40 +0000

    Update siteProps with personal information and add animation to social icons

--- src/App.jsx
+++ src/App.jsx
@@ -24,10 +24,10 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
+  name: "Amit Suryawanshi",
+  title: "Full Stack Developer",
+  email: "patilamit8605@gmail.com",
+  gitHub: "https://github.com/surya-wanshi-amit",
   instagram: "microsoft",
   linkedIn: "satyanadella",
   medium: "",
